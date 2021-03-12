ce2015a8535f1a9e0ada9f2ae674ae9f2104ee61
nlabrise@gmail.com <nlabrise@gmail.com>
2021-03-12 13:55:55 -0500

    divided giant class into two classes
    Took the giant Spotify API class and turned it into a child of the new class, Results.

--- spotify_search.py
+++ spotify_search.py
@@ -8,91 +8,15 @@ client_id = "b75dfd2aa89f4ed985eb2f20638c0633"
 client_secret = "6f994b2726774f8d93146681bb7a7fcd"
 
 
-# divide Spotify_API class into smaller, more managable classes
+class Query_Search(object):
 
-# create r function this way it is not repeated so much
+	base_url = 'https://api.spotify.com/v1/artists/'
 
-
-class Spotify_API(object):
-	access_token = None
-	access_token_expires = datetime.datetime.now()
-	access_token_did_expire = True
-	# client_id = None
-	# client_secret = None
-	base_url = f'https://api.spotify.com/v1/artists/'
-	
-	def __init__(self, client_id, client_secret, *args, **kwargs):
-		self.client_id = client_id
-		self.client_secret = client_secret
-		
-	def get_client_credentials(self):
-		client_id = self.client_id
-		client_secret = self.client_secret
-		if client_secret == None or client_id == None:
-			raise Exception("You must set client_id and client_secret.")
-		client_creds = f'{client_id}:{client_secret}'
-		client_creds_b64 = base64.b64encode(client_creds.encode())        
-		return client_creds_b64.decode()
-		
-	def get_token_headers(self):
-		client_creds_b64 = self.get_client_credentials()
-		return {
-			'Authorization':f'Basic {client_creds_b64}'
-		}
-
-	def perform_auth(self):
-		token_url = 'https://accounts.spotify.com/api/token'
-		token_data = {'grant_type':'client_credentials'}
-		token_headers = self.get_token_headers()
-		r = requests.post(token_url, data=token_data, headers=token_headers)
-
-		# OR instead to cut down code we can do the following: 
-		# r = requests.post('https://accounts.spotify.com/api/token', data={'grant_type':'client_credentials'}, headers=self.get_token_headers())
-
-		if r.status_code not in range(200, 299):
-			raise Exception("Could not authenticate client.")
-		token_data = r.json()
-		now = datetime.datetime.now()
-		self.access_token = token_data['access_token']
-		expires_in = token_data['expires_in']
-		self.access_token_expires = now + datetime.timedelta(seconds=expires_in)
-	
-	def get_access_token(self):
-		if self.access_token_expires < datetime.datetime.now() or self.access_token == None:
-			self.perform_auth()
-			return self.get_access_token()
-		return self.access_token
-	
-	def get_resource_header(self):
-		access_token = self.get_access_token()
-		headers = {
-			'Authorization':f'Bearer {access_token}'
-		}
-		return headers
-
-#This would be a good place to start a new class that takes the creds of a creds class and insert them into an artist_info class. 
-
-	def base_search(self, query_params):
-		headers = self.get_resource_header()
-		endpoint = 'https://api.spotify.com/v1/search'
-		lookup_url = f'{endpoint}?{query_params}'
-		r = requests.get(lookup_url, headers=headers)
-		if r.status_code not in range(200,299):
-			return {}
-		return r.json()
-		
 	def search(self, query=None, operator=None, operator_query=None, search_type='artist'):
-		# print(query)
 		if query == None:
 			raise Exception("A query is required.")
 		if isinstance(query, dict):
 			query = ' '.join([f'{k}:{v}' for k,v in query.items()])
-
-		print(operator)
-		print(operator_query)
-
-		# wtf is this below!
-
 		# if operator != None and operator_query != None:
 		# 	if operator.lower() == 'or' or operator.lower() == 'not':
 		# 		operator = operator.upper()
@@ -100,6 +24,15 @@ class Spotify_API(object):
 		# 			query = f'{query} {operator} {operator_query}'
 		query_params = urlencode({'q':query, 'type':search_type.lower()})
 		return self.base_search(query_params)
+
+	def base_search(self, query_params):
+		headers = self.get_resource_header() #if we split this into another class, we need to change self
+		endpoint = 'https://api.spotify.com/v1/search'
+		lookup_url = f'{endpoint}?{query_params}'
+		r = requests.get(lookup_url, headers=headers)
+		if r.status_code not in range(200,299):
+			return {}
+		return r.json()
 
 	def get_artist_albums(self, artist_id):
 		base_url = self.base_url
@@ -122,6 +55,60 @@ class Spotify_API(object):
 			return [top_tracks['tracks'][x]['name'] for x in range(len(top_tracks['tracks']))]
 		else:
 			return [top_tracks['tracks'][x]['name'] for x in range(10)] # returns artist's top 10 tracks in a list
+
+
+class Spotify_API(Query_Search):
+	access_token = None
+	access_token_expires = datetime.datetime.now()
+	access_token_did_expire = True
+	# client_id = None
+	# client_secret = None
+	
+	
+	def __init__(self, client_id, client_secret, *args, **kwargs):
+		self.client_id = client_id
+		self.client_secret = client_secret
+		
+	def get_client_credentials(self):
+		# client_id = self.client_id
+		# client_secret = self.client_secret
+		if client_secret == None or client_id == None:
+			raise Exception("You must set client_id and client_secret.")
+		client_creds = f'{client_id}:{client_secret}'
+		client_creds_b64 = base64.b64encode(client_creds.encode())
+		return client_creds_b64.decode()
+		
+	def get_token_headers(self):
+		client_creds_b64 = self.get_client_credentials()
+		return {
+			'Authorization':f'Basic {client_creds_b64}'
+		}
+
+	def perform_auth(self):
+		token_url = 'https://accounts.spotify.com/api/token'
+		token_data = {'grant_type':'client_credentials'}
+		token_headers = self.get_token_headers()
+		r = requests.post(token_url, data=token_data, headers=token_headers)
+		if r.status_code not in range(200, 299):
+			raise Exception("Could not authenticate client.")
+		token_data = r.json()
+		now = datetime.datetime.now()
+		self.access_token = token_data['access_token']
+		expires_in = token_data['expires_in']
+		self.access_token_expires = now + datetime.timedelta(seconds=expires_in)
+	
+	def get_access_token(self):
+		if self.access_token_expires < datetime.datetime.now() or self.access_token == None:
+			self.perform_auth()
+			return self.get_access_token()
+		return self.access_token
+	
+	def get_resource_header(self):
+		access_token = self.get_access_token()
+		headers = {
+			'Authorization':f'Bearer {access_token}'
+		}
+		return headers
 
 
 def recent_albums_header():
